c184e8402b70e287f01fa4f5a86367afa7c3f04d
Lekan <olufeyisanlekan@gmail.com>
2025-05-09 16:15:34 +0100

    added user DTO

---
+++ CsKmsBackend.Application/DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsKmsBackend.Application.DTOs
+{
+	public record UserDTO(
+		int Id,
+		[Required] string Name,
+		[Required] string Email,
+		[Required] string Role,
+		[Required, MinLength(1, ErrorMessage = "Please pick one department")] List<int> Departments
+		);
+}
