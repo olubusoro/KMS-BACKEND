34c5490b84db7dd4f491ca6c410dbfbdaa6d1894
Lekan <olufeyisanlekan@gmail.com>
2025-05-13 22:56:19 +0100

    added authservice

---
+++ CsKmsBackend.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using CsKmsBackend.Application.DTOs;
+using CsKmsBackend.Domain.Models;
+
+namespace CsKmsBackend.Application.Interfaces
+{
+	public interface IAuthService
+	{
+		Task<ResponseKms> LoginAsync(LoginDTO request);
+	}
+}
