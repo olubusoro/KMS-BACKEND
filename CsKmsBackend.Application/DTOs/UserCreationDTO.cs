3a2190ca84eba6f7c8a497bd6a40ad72934f433f
Lekan <olufeyisanlekan@gmail.com>
2025-05-13 12:00:08 +0100

    added DTO to create users and conversions for Entities(models) and DTO

---
+++ CsKmsBackend.Application/DTOs/UserCreationDTO.cs
@@ -0,0 +1,14 @@
+﻿using CsKmsBackend.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CsKmsBackend.Application.DTOs
+{
+	public record UserCreationDTO(
+		int Id,
+		[Required] string Name,
+		[Required, EmailAddress] string Email,
+		[Required, MinLength(1, ErrorMessage = "Password must be greather than 8 characters")] string Password,
+		[Required] UserRole Role,
+		[Required, MinLength(1, ErrorMessage = "Please pick one department")] List<int> Departments
+		);
+}
