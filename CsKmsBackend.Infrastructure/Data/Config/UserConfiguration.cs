f14e6ef5373c183c3c52017d190521d592690672
Lekan <olufeyisanlekan@gmail.com>
2025-05-13 12:09:28 +0100

    added db context cofiguration to handle user role as string

---
+++ CsKmsBackend.Infrastructure/Data/Config/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CsKmsBackend.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace CsKmsBackend.Infrastructure.Data.Config
+{
+	public class UserConfiguration : IEntityTypeConfiguration<User>
+	{
+		public void Configure(EntityTypeBuilder<User> builder)
+		{
+			// Configure MyClass to store the Role enum as a string
+
+			builder.Property(e => e.Role).HasConversion<string>();
+
+
+		}
+	}
+}
