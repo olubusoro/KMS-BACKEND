449244ea1b9a1352db975f973248b2739bfc3427
Lekan <olufeyisanlekan@gmail.com>
2025-05-21 16:40:15 +0100

    added post and post attachment models

---
+++ CsKmsBackend.Domain/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿namespace CsKmsBackend.Domain.Models
+{
+	public class Post
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string? Description { get; set; }
+		public string Content { get; set; }
+		public string Visibility { get; set; }
+		public int? CategoryId { get; set; } // int? because no category yet
+		//public Category Category { get; set; }
+		public int? UserId { get; set; }
+		public User CreatedBy { get; set; }
+		public IList<PostAttachment> Attachments { get; set; } = [];
+		//public IList<AccessRequest> AccessRequests { get; set; } = new List<AccessRequest>();
+		//public IList<Log> Logs { get; set; } = new List<Log>();
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+		public DateTime? UpdatedAt { get; set; }
+	}
+}
---
+++ CsKmsBackend.Domain/Models/PostAttachment.cs
@@ -0,0 +1,14 @@
+﻿namespace CsKmsBackend.Domain.Models
+{
+	public class PostAttachment
+	{
+		public int Id { get; set; }
+		public string OriginalFileName { get; set; }
+		public string FileName { get; set; }
+		public string ContentType { get; set; }
+		public string FilePath { get; set; }
+
+		public int PostId { get; set; }
+		public Post Post { get; set; }
+	}
+}
