33dc5c4dc9903ec35d4ac5c23d7a4088de9f9ad9
Lekan <olufeyisanlekan@gmail.com>
2025-05-21 16:49:05 +0100

    added post crud endpoints and migrations

---
+++ CsKmsBackend.Infrastructure/Data/Migrations/20250521152451_PostAndPostAttachment.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CsKmsBackend.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostAndPostAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Visibility = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostAttachments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OriginalFileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostAttachments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostAttachments_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostAttachments_PostId",
+                table: "PostAttachments",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostAttachments");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
---
+++ CsKmsBackend.Presentation/Controllers/PostsController.cs
@@ -0,0 +1,47 @@
+﻿using CsKmsBackend.Application.DTOs;
+using CsKmsBackend.Application.Interfaces;
+using CsKmsBackend.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsKmsBackend.Presentation.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PostsController(IPostService postService) : ControllerBase
+	{
+		[HttpPost]
+		public async Task<ActionResult<ResponseKms>> CreatePost([FromForm]PostCreationDTO postCreationDTO)
+		{
+			var result = await postService.CreatePostAsync(postCreationDTO);
+			return result.Flag ? Ok(result) : BadRequest(result);
+		} 
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<PostDTO>>> GetAllPosts()
+		{
+			return Ok(await postService.GetAllPostsAsync());
+		}
+
+		[HttpGet("{id:int}")]
+		public async Task<ActionResult<PostDTO>> GetPost(int id)
+		{
+			var post = await postService.GetPostAsync(id);
+			return post is not null ? Ok(post) : NotFound();
+		}
+
+		[HttpPut]
+		public async Task<ActionResult<ResponseKms>> UpdatePost(PostUpdateDTO postUpdateDTO)
+		{
+			if(!ModelState.IsValid) return BadRequest(ModelState);
+			var result = await postService.UpdatePostAsync(postUpdateDTO);
+			return result.Flag ? Ok(result) : BadRequest(result);
+		}
+
+		[HttpDelete("{id:int}")]
+		public async Task<ActionResult<ResponseKms>> DeletePost(int id)
+		{
+			var result = await postService.DeletePostAsync(id);
+			return result.Flag ? Ok(result) : BadRequest(result);
+		}
+	}
+}
