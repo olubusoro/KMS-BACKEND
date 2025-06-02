45b536cf1ae629c4e245eb2ec08cacb26defb27d
Lekan <olufeyisanlekan@gmail.com>
2025-06-02 11:44:12 +0100

    added log and log enums

---
+++ CsKmsBackend.Domain/Models/Enums/LogEnums.cs
@@ -0,0 +1,18 @@
+﻿namespace CsKmsBackend.Domain.Models.Enums
+{
+	public enum ActionType
+	{
+		Create,
+		Update,
+		Delete,
+		RequestAccess,
+		ApproveAccess,
+		DenyAccess
+	}
+
+	public enum EntityType
+	{
+		Post,
+		AccessRequest
+	}
+}
