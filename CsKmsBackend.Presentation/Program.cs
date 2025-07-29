cfcd94e92cd98768c5f8a5ebaff5388f6a0c0387
tealjo <105678321+tealjo@users.noreply.github.com>
2025-07-30 00:04:23 +0100

    made swagger UI appear in production

--- CsKmsBackend.Presentation/Program.cs
+++ CsKmsBackend.Presentation/Program.cs
@@ -46,11 +46,8 @@ builder.Services.AddInfrastructureService(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 if (args.Contains("--seed"))
 {
